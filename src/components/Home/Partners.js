b524f4e5a87722aa8cb3deb707bc977d06d2d38f
haciyeff03 <haciyevqurban97@gmail.com>
2025-07-16 01:59:41 +0400

    our partners language fixed

--- src/components/Home/Partners.js
+++ src/components/Home/Partners.js
@@ -7,15 +7,17 @@ import Github from "@/assets/images/github_logo.svg";
 import Microsoft from "@/assets/images/microsoft_logo.svg";
 import Redhat from "@/assets/images/redhat_logo.svg";
 import Image from "next/image";
+import { useTranslations } from 'next-intl';
 
 
 
 const Partners = () => {
     const images = [Autodesk, DesignFoundation, DigitalMarketing, Fortinet, Github, Microsoft, Redhat];
+        const t = useTranslations('OurPartners');
 
   return (
     <section className="w-full max-w-[1350px] m-auto flex flex-col gap-[20px]"> 
-        <h2 className="px-[20px] text-[32px] sm:text-[36px] md:text-[48px] text-center">Bizim <span className="font-bold">Partnyorlarımız</span></h2>
+        <h2 className="px-[20px] text-[32px] sm:text-[36px] md:text-[48px] text-center"> <span className="font-bold"> {t('OurPartners')}</span></h2>
         <div className="relative w-full overflow-hidden">
           <Marquee className="[--duration:30s] gap-[20px]">
             {images.map((image, index) => (
